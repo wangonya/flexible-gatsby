f525404bac8561434193f9c7facc6bd500724683
kelvin <kelvin@jenga-agency.com>
2022-02-10 18:59:15 +0300

    set posts to 10 per page

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,7 +58,7 @@ exports.createPages = ({ graphql, actions }) => {
     })
 
     // Create blog post list pages
-    const postsPerPage = 5
+    const postsPerPage = 10
     const numPages = Math.ceil(posts.length / postsPerPage)
 
     Array.from({ length: numPages }).forEach((_, i) => {
